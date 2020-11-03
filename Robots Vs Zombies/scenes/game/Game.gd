db0d9b380bab26e15aabd5a5aa98cab82c715d2f
Oswaldo <oswaldo@estudiantec.cr>
2020-11-02 23:37:39 -0600

    implementando generador

--- Robots Vs Zombies/scenes/game/Game.gd
+++ Robots Vs Zombies/scenes/game/Game.gd
@@ -17,3 +17,40 @@ func _unhandled_input(event):
 		print("no turno")
 	
 	return turno
+
+# ----------------------------------------------------------
+
+
+const enemies = [
+	preload("res://scenes/entidades/zombies/EnemyA/EnemigoA.tscn")
+]
+
+func _ready():
+	spawn()
+	pass
+
+func choose(choises):
+	randomize()
+	var rand_index = randi() % choises.size()
+	return choises[rand_index]
+	pass
+	
+func create_timer(tiempo):
+	var timer = Timer.new()
+	timer.set_wait_time(tiempo)
+	timer.set_one_shot(true)
+	timer.connect("timeout", timer, "queue_free")
+	add_child(timer)
+	timer.start()
+	return timer
+	pass
+	
+func spawn():
+	while true:
+		randomize()
+		
+		var enemy = choose(enemies).instance()
+		enemy.position = get_node("Craigh").global_position
+		add_child(enemy)
+		yield(create_timer(rand_range(5, 10)), "timeout")
+	pass
