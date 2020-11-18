318664be24df086b6f9a1325f305dc8ae488256e
Cristhian <crisesqui908@gmail.com>
2020-11-18 12:46:54 -0600

    Implementación de habilidades de Craigh

--- Robots Vs Zombies/scenes/entidades/personajes/craigh/Craigh.gd
+++ Robots Vs Zombies/scenes/entidades/personajes/craigh/Craigh.gd
@@ -1,6 +1,7 @@
 extends "res://scripts/entidades/personajes/Personaje.gd"
 
 func _ready():
+	cola_items.append("Kaboom")
 	_rango = 4
 	pass
 	
@@ -9,9 +10,20 @@ func habDobleRango():
 	get_node("sound_item").play()
 	#Duplica el rango
 	_rango *= 2
+	print("Rango aumentado a " , _rango)
+
+func aumentarVision():
+	#Aumenta la visión de Craigh
+	$Camera2D.zoom.y += 0.5
+	$Camera2D.zoom.x += 0.5
+	print("Visión aumentada")
 
 func _on_Craigh_area_entered(area):
 	if area.is_in_group("items_craigh"):
-		habDobleRango()
-		print("Rango aumentado a " , _rango)
+		if area.nombre == "DobleRango":
+			habDobleRango()
+		
+		elif area.nombre == "AumentoVision":
+			aumentarVision()
+		
 
